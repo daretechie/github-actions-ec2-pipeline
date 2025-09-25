3f502b072e7f452016a6ecc2d0b9c62b0b12a1aa
daretechie <deeprince2020@gmail.com>
2025-09-25 18:30:21 +0100

    checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   env: {
     node: true,
     es2022: true,
     jest: true,
   },
   rules: {
-    'no-console': 'off',
+    "no-console": "off",
   },
 };
